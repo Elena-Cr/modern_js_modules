64ce528f590f14cbe3e7f8bf2dc9d504efb33c17
Elena Cristescu <elenacristescu11@gmail.com>
2024-02-06 17:28:55 +0100

    other importing and exporting practices

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 //Inporting Module
-import './shoppingCart.js';
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 console.log('Importing module');
+import * as ShopingCart from './shoppingCart.js';
+
+//addToCart('bread', 5);
