786e7631d159cda940bb55776798bddba4018e6d
Elena Cristescu <elenacristescu11@gmail.com>
2024-02-07 17:00:49 +0100

    extra examples of exporting and importing

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 //Inporting Module
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
-console.log('Importing module');
-import * as ShopingCart from './shoppingCart.js';
-
 //addToCart('bread', 5);
+
+console.log('Importing module');
+// import * as ShopingCart from './shoppingCart.js';
+
+import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+
+add('bread', 5);
+// console.log(ShopingCart.totalPrice);
+console.log(price);
