f525f600e6bdde63aa26b9e3afc88a5bc81347cd
Elena Cristescu <elenacristescu11@gmail.com>
2024-02-08 16:48:17 +0100

    download leaflet and lodash library using npm

--- script.js
+++ script.js
@@ -2,11 +2,32 @@
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 //addToCart('bread', 5);
 
-console.log('Importing module');
-// import * as ShopingCart from './shoppingCart.js';
+// console.log('Importing module');
+// // import * as ShopingCart from './shoppingCart.js';
 
-import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 
-add('bread', 5);
-// console.log(ShopingCart.totalPrice);
-console.log(price);
+// add('bread', 5);
+// // console.log(ShopingCart.totalPrice);
+// console.log(price);
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    {
+      product: 'bread',
+      quantity: 5,
+    },
+    {
+      product: 'pizza',
+      quantity: 5,
+    },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
